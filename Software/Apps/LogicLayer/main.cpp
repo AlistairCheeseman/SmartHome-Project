4b824a9e94d90e2ae9b4dffff964c626ac33abf8
root <ali@kmxsoftware.co.uk>
2015-02-01 21:10:34 +0000

    added blank C++ Logic Layer Project

---
+++ Software/Apps/LogicLayer/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: alistair
+ *
+ * Created on 22 January 2015, 15:11
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
