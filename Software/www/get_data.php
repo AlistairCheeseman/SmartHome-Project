0e44aed797fb57c17e74f19cea9ef81780758d37
Alistair Cheeseman <ac441@sussex.ac.uk>
2015-02-24 13:09:12 +0000

    updated get_data to get a more diverse set of data

--- Software/www/get_data.php
+++ Software/www/get_data.php
@@ -1,39 +1,28 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 include "functions.php";
-
-        
-     $db = connectDB();    
-         //    $statement =    $db->prepare("SELECT DATE(moment) as moment, powerReading, powerFactor  FROM 'PowerLog'"); 
-       $statement =    $db->prepare("SELECT moment, powerReading, powerFactor  FROM 'PowerLog'"); 
-             $statement->execute();
-             $results=$statement->fetchAll(PDO::FETCH_ASSOC);
-             echo json_encode($results);
-             
-    //    echo "[";
-    //          foreach ($result as $row)
-    // {
-    //             echo "[\"".$row['moment']."\",".$row['powerReading'].",".$row['powerFactor']."]"; 
-    //          }
-    //         echo "]";
-      
-     
-         
-             
-             
-             
-             
-$db = null;
-
-
-
-
-
-
-
+$DBView = gettext($_GET['view']);
+if ($DBview != NULL) {
+    $db = connectDB();
+    if ($DBView == "power") {
+        //    $statement =    $db->prepare("SELECT DATE(moment) as moment, powerReading, powerFactor  FROM 'PowerLog'"); 
+        $statement = $db->prepare("SELECT moment, powerReading, powerFactor  FROM 'PowerLog'");
+        $statement->execute();
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($results);
+    } elseif ($DBView == "Rooms") {
+        $statement = $db->prepare("SELECT Name  FROM 'Rooms'");
+        $statement->execute();
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($results);
+    }
+    $db = null;
+} else {
+    echo "missing or wrong parameters";
+}
 ?>
