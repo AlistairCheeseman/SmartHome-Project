bf765ce8273984dfd77a25297b0fb1d4fe7dede3
Alistair Cheeseman <ac441@sussex.ac.uk>
2015-03-09 19:17:41 +0000

    added room listing populating by javascript (json ajax requests)

---
+++ Software/www/js/temp.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ $(document).ready(function (){
+                $.getJSON('get_data.php?view=Rooms', function (json)
+                {
+                    $.each(json, function(key,value) {
+                var row = $("<tr />");
+    $("#personDataTable").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
+    row.append($("<td>" + value['Name'] + "</td>"));
+    row.append($("<td>" + value['sensorC'] + "</td>"));
+      row.append($("<td>" + "<a href='rooms/edit?id=" + value['Id']+"'>"+"Edit" +"</a>"+ "</td>"));
+  //  row.append($("<td><a href=/rooms/edit?id=" + value['Id'] + ">Edit</a></td>"));
+    row.append($("<td><a href='/rooms/delete?id=" + value['Id'] + "' >Delete</a></td>"));
+                    }); 
+                });
+});
