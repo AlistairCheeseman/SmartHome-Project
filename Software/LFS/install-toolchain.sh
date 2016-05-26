c018d1f22d14a0a39cdb599c8b8daff086e3318c
AlistairCheeseman <ac441@sussex.ac.uk>
2016-05-26 19:53:14 +0100

    merged mirror and non mirror toolchain scripts added ssh helper to specify ssh key location

--- Software/LFS/install-toolchain.sh
+++ Software/LFS/install-toolchain.sh
@@ -36,10 +36,12 @@ export FLOAT=hard
 
 export MAKEINFO_LOC=$(which makeinfo)
 
+export USE_MIRROR=TRUE
 
 
 #create the sysroot directory and link usr to the local folder to ensure everything is installed in root
 chmod -R 777 $ROOTDIR
+rm -Rf $ROOTDIR/*
 mkdir -p $SRCDIR
 mkdir -p $SYSROOT
 
@@ -47,13 +49,42 @@ ln -s . $SYSROOT/usr
 
 cd $SRCDIR
 #wget ftp://sourceware.org/pub/binutils/snapshots/binutils-2.24.51.tar.bz2
+if [ $USE_MIRROR == 'FALSE' ]; then
+
 wget http://ftp.gnu.org/gnu/binutils/binutils-2.25.tar.bz2
 wget ftp://ftp.mirrorservice.org/sites/sourceware.org/pub/gcc/releases/gcc-4.9.1/gcc-4.9.1.tar.bz2
 wget http://ftp.gnu.org/gnu/glibc/glibc-2.19.tar.bz2
 wget https://gmplib.org/download/gmp/gmp-6.0.0a.tar.bz2
-wget http://www.mpfr.org/mpfr-current/mpfr-3.1.3.tar.bz2
+wget http://www.mpfr.org/mpfr-current/mpfr-3.1.4.tar.bz2
 wget ftp://ftp.gnu.org/gnu/mpc/mpc-1.0.2.tar.gz
 wget http://www.mr511.de/software/libelf-0.8.13.tar.gz
+
+wget http://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.9.tar.gz
+wget https://git.fedorahosted.org/cgit/libaio.git/snapshot/libaio-0.3.109.tar.gz
+wget http://c-ares.haxx.se/download/c-ares-1.10.0.tar.gz
+wget http://www.openssl.org/source/openssl-1.0.1i.tar.gz
+wget http://zlib.net/zlib-1.2.8.tar.gz
+
+wget http://arago-project.org/git/projects/?p=am33x-cm3.git\;a=blob_plain\;f=bin/am335x-pm-firmware.bin\;hb=HEAD -O am335x-pm-firmware.bin
+else
+
+wget https://filestore.kmxsoftware.co.uk/binutils-2.25.tar.bz2
+wget https://filestore.kmxsoftware.co.uk/gcc-4.9.1.tar.bz2
+wget https://filestore.kmxsoftware.co.uk/glibc-2.19.tar.bz2
+wget https://filestore.kmxsoftware.co.uk/gmp-6.0.0a.tar.bz2
+wget https://filestore.kmxsoftware.co.uk/mpfr-3.1.3.tar.bz2
+wget https://filestore.kmxsoftware.co.uk/mpc-1.0.2.tar.gz
+wget https://filestore.kmxsoftware.co.uk/libelf-0.8.13.tar.gz
+
+wget https://filestore.kmxsoftware.co.uk/ncurses-5.9.tar.gz
+wget https://filestore.kmxsoftware.co.uk/libaio-0.3.109.tar.gz
+wget https://filestore.kmxsoftware.co.uk/c-ares-1.10.0.tar.gz
+wget https://filestore.kmxsoftware.co.uk/openssl-1.0.1i.tar.gz
+wget https://filestore.kmxsoftware.co.uk/zlib-1.2.8.tar.gz
+
+wget https://filestore.kmxsoftware.co.uk/am335x-pm-firmware.bin -O am335x-pm-firmware.bin
+fi
+
 
 echo "Extracting Files"
 tar -xf binutils-2.25.tar.bz2
@@ -63,6 +94,13 @@ tar -xf gmp-6.0.0a.tar.bz2
 tar -xf mpfr-3.1.3.tar.bz2
 tar -xf mpc-1.0.2.tar.gz
 tar -xf libelf-0.8.13.tar.gz
+
+tar -xf zlib-1.2.8.tar.gz
+tar -xf openssl-1.0.1i.tar.gz
+tar -xf c-ares-1.10.0.tar.gz
+tar -xf libaio-0.3.109.tar.gz
+tar -xf ncurses-5.9.tar.gz
+
 echo "Cleaning Directory Structure"
 mv -v binutils-2.25 binutils
 mv -v gcc-4.9.1 gcc-src
@@ -71,6 +109,12 @@ mv -v gmp-6.0.0 gmp
 mv -v mpfr-3.1.3 mpfr
 mv -v mpc-1.0.2 mpc
 mv -v libelf-0.8.13 libelf
+mv -v zlib-1.2.8 zlib
+mv -v openssl-1.0.1i openssl
+mv -v c-ares-1.10.0 c-ares
+mv -v libaio-0.3.109 libaio
+mv -v ncurses-5.9 ncurses
+
 
 mv mpfr gcc-src/mpfr
 mv gmp gcc-src/gmp
@@ -85,29 +129,36 @@ rm gmp-6.0.0a.tar.bz2
 rm mpfr-3.1.3.tar.bz2
 rm mpc-1.0.2.tar.gz
 rm libelf-0.8.13.tar.gz
+
+rm zlib-1.2.8.tar.gz
+rm openssl-1.0.1i.tar.gz
+rm ncurses-5.9.tar.gz
+rm c-ares-1.10.0.tar.gz
+rm libaio-0.3.109.tar.gz
+
 echo "Getting LINUX Kernel from GIT"
 git config --global user.email “ali@kmxsoftware.co.uk”
 
-#repository moved.
-#git clone https://github.com/beagleboard/kernel.git
+if [ $USE_MIRROR == 'FALSE' ]; then
 git clone https://github.com/beagleboard/linux.git
+else
+GIT_SSH=${DIR}/ssh_helper git clone git@git.kmxsoftware.co.uk:extern/linux.git
+fi
 #cd kernel
 cd linux
 #with new git repository this may not be needed anymore
 #git checkout origin/3.13 -b 3.13
 git checkout origin/3.14 -b 3.14
-#
-#./patch.sh
-#cp configs/beaglebone kernel/arch/arm/configs/beaglebone_defconfig
-#wget http://arago-project.org/git/projects/?p=am33x-cm3.git\;a=blob_plain\;f=bin/am335x-pm-firmware.bin\;hb=HEAD -O kernel/firmware/am335x-pm-firmware.bin
-wget http://arago-project.org/git/projects/?p=am33x-cm3.git\;a=blob_plain\;f=bin/am335x-pm-firmware.bin\;hb=HEAD -O firmware/am335x-pm-firmware.bin
+mv $SRCDIR/am335x-pm-firmware.bin firmware/am335x-pm-firmware.bin
 #kernel
 echo "Building Kernel Headers"
+echo -e '\033]2;Building Kernel Headers\007'
 cd $SRCDIR/linux
 #make ARCH=arm beaglebone_defconfig  2>&1 | tee make.out
 make ARCH=arm bb.org_defconfig
 make ARCH=arm INSTALL_HDR_PATH=$SYSROOT headers_install  2>&1 | tee -a make.out
 echo "Building BinUtils"
+echo -e '\033]2;Building Binutils\007'
 #binutils
 mkdir -pv $SRCDIR/binutils-build
 cd $SRCDIR/binutils-build
@@ -122,6 +173,7 @@ make install 2>&1 | tee -a make.out
 
 #gcc pass 1
 echo "GCC - Pass 1"
+echo -e '\033]2;GCC Pass 1\007'
 mkdir -pv $SRCDIR/gcc-build-1
  cd $SRCDIR/gcc-build-1
  ../gcc-src/configure \
@@ -155,6 +207,7 @@ mkdir -pv $SRCDIR/gcc-build-1
 make 2>&1 | tee make.out
 make install 2>&1 | tee -a make.out
 echo "GLibC"
+echo -e '\033]2;GLIBC\007'
 #glibc-build
 mkdir $SRCDIR/glibc-build
 cd $SRCDIR/glibc-build
@@ -177,6 +230,7 @@ LD=${CROSS_COMPILE}ld \
 
 
 echo "GCC - Pass 2"
+echo -e '\033]2;GCC Pass 2\007'
   mkdir $SRCDIR/gcc-build-2
  cd $SRCDIR/gcc-build-2
  #gcc-build-2
@@ -207,22 +261,16 @@ export CC=arm-unknown-linux-gnueabihf-gcc
 
 
 echo "ZLIB"
- cd $SRCDIR
- wget http://zlib.net/zlib-1.2.8.tar.gz
- tar -xf zlib-1.2.8.tar.gz
-rm zlib-1.2.8.tar.gz
- cd zlib-1.2.8
+echo -e '\033]2;ZLIB\007'
+ cd $SRCDIR/zlib
  ./configure  2>&1 | tee configure.out
  make  2>&1 | tee make.out
  make install prefix=${SYSROOT}
 echo "OpenSSL"
+echo -e '\033]2;OpenSSL\007'
  ##OPENSSL is needed for opennsh and mosquitto
  unset CROSS_COMPILE
- cd $SRCDIR
-wget http://www.openssl.org/source/openssl-1.0.1i.tar.gz
-tar -xf openssl-1.0.1i.tar.gz
-rm openssl-1.0.1i.tar.gz
-cd openssl-1.0.1i
+ cd $SRCDIR/openssl
 ##MUST BE BUILT WITH -fPIC otherwise OpenSSH fails.
 #if CROSS_COMPILE has been set:
 #CC="gcc -fPIC" ./Configure dist --prefix=${SYSROOT} zlib-dynamic  2>&1 | tee configure.out
@@ -236,33 +284,24 @@ make install  2>&1 | tee -a make.out
 
 
 echo "C-Ares"
+echo -e '\033]2;C-Ares\007'
 #C-ARES IS NEEDED FOR MOSQUITTO
-cd $SRCDIR
-wget http://c-ares.haxx.se/download/c-ares-1.10.0.tar.gz
-tar -xf c-ares-1.10.0.tar.gz
-rm c-ares-1.10.0.tar.gz
-cd c-ares-1.10.0
+cd $SRCDIR/c-ares
 ./configure --host=$TARGET --prefix=/  2>&1 | tee configure.out
 make  2>&1 | tee make.out
 make install DESTDIR=${SYSROOT} 2>&1 | tee -a make.out
 
 
 echo "Libaio"
-cd $SRCDIR
-wget https://git.fedorahosted.org/cgit/libaio.git/snapshot/libaio-0.3.109.tar.gz
-tar -xf libaio-0.3.109.tar.gz
-rm libaio-0.3.109.tar.gz
-cd libaio-0.3.109
+echo -e '\033]2;Libaio\007'
+cd $SRCDIR/libaio
 make 2>&1 | tee make.out
 #ignore warnings about git. as this was not pulled from a git repo
 make install prefix=$SYSROOT 2>&1 | tee -a make.out
 
 echo "ncurses"
-cd $SRCDIR
-wget http://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.9.tar.gz
-tar -xf ncurses-5.9.tar.gz
-rm ncurses-5.9.tar.gz
-cd ncurses-5.9
+echo -e '\033]2;ncurses\007'
+cd $SRCDIR/ncurses
 ./configure --prefix=/ --host=$TARGET  2>&1 | tee configure.out
 make  2>&1 | tee make.out
 make install DESTDIR=$SYSROOT  2>&1 | tee -a make.out
